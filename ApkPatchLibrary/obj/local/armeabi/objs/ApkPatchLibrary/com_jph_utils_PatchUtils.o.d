a1c9d718993e92e87e4a61fa038e887142eb8fd8
crazycodeboy <crazycodeboy@gmail.com>
2016-04-28 16:43:35 +0800

    update

---
+++ ApkPatchLibrary/obj/local/armeabi/objs/ApkPatchLibrary/com_jph_utils_PatchUtils.o.d
@@ -0,0 +1,32 @@
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/obj/local/armeabi/objs/ApkPatchLibrary/com_jph_utils_PatchUtils.o: \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/com_jph_utils_PatchUtils.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/bzlib.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/bzlib_private.h \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/bzlib.h \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/crctable.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/compress.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/decompress.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/randtable.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/blocksort.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/huffman.c \
+ E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/com_jph_utils_PatchUtils.h
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/bzlib.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/bzlib_private.h:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/bzlib.h:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/crctable.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/compress.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/decompress.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/randtable.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/blocksort.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/bzip2/huffman.c:
+
+E:/JiaPengHui/workplace/GitRepository/IncrementalUpdate/ApkPatchLibrary/jni/com_jph_utils_PatchUtils.h:
---
+++ IncrementalUpdate-Android/app/src/main/java/com/jph/utils/PatchUtils.java
@@ -0,0 +1,19 @@
+package com.jph.utils;
+
+/**
+ * APK Patch工具类
+ */
+public class PatchUtils {
+
+	/**
+	 * native方法 使用路径为oldApkPath的apk与路径为patchPath的补丁包，合成新的apk，并存储于newApkPath
+	 * 
+	 * 返回：0，说明操作成功
+	 * 
+	 * @param oldApkPath 示例:/sdcard/old.apk
+	 * @param newApkPath 示例:/sdcard/new.apk
+	 * @param patchPath  示例:/sdcard/xx.patch
+	 * @return
+	 */
+	public static native int patch(String oldApkPath, String newApkPath, String patchPath);
+}
